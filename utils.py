bf4cf33e757c8c0390901f405f667692b4113f84
Vladimir <vovcrane@mail.ru>
2024-04-05 20:11:13 +0300

    Implement universal drawing function

--- utils.py
+++ utils.py
@@ -567,4 +567,178 @@ def draw_dynamic(image_filename,
         print('Изображение в png-формате сохранено')
         
     return result_image
+
+
+
+
+
+
+
+
+
+
+
+
+
+def draw_ultra(image_filename,
+               save_image_name,
+               emoji_width=50,
+               emoji_resolution=50,
+               background_color=(0, 0, 0),
+               emojis=None, 
+               emoji_dict_name='classic_black', 
+               dynamic_background=False,
+               disp=True):
+    """
+    Функция для рисования картинки с динамическим фоном
+    (для каждого смайлика подбирается собственный цвет фона)
+
+    PARAMETERS
+    ----------
+    image_filename : str
+        | название файла картинки, которую необходимо 
+          нарисовать смайликами. Файл обязатнльно должен 
+          находиться в папке input/ 
     
+    save_image_name : str
+        | имя картинки для сохранения
+    
+    emoji_width : int
+        | ширина итоговой картинки (в смайликах)
+    
+    emoji_resolution : int
+        | каждый смайлик рисуется в квадратной области, 
+          и этот параметр задает размер стороны квадрата
+          в пикселях
+    
+    background_color : (int, int, int)
+        | фоновый цвет итоговой картинки из смайликов
+    
+    emojis : str или None
+        | смайлики, которыми необходимо нарисовать картинку:
+          - если указана строка, состоящаяя из смайликов через
+            пробел, то для них будет построен словарь, на основе 
+            которого и будет рисоваться картинка (при этом 
+            параметр emoji_dict не учитывается);
+          - если None, в качестве словаря смайликов будет
+            использоваться сохраненный в папке emoji_dicts/
+            словарь с названием, указанным в параметре
+            emoji_dict
+    
+    emoji_dict_name : str или None
+        | название словаря смайликов из папки emoji_dicts/
+          - если указана строка, а параметр emojis = None,
+            то для рисования используется указанный словарь;
+          - если None, для рисования используются смайлики из
+            параметра emojis (но если этот параметр = None,
+            будет выброшена ошибка)
+
+    dynamic_background : bool
+        | использовать ли динамическое создание фона, при
+          котором для каждого смайлика отрисовывается собственный 
+          цвет фона, в сочетании с которым он становится еще ближе
+          к исходной картинке
+    
+    disp : bool
+        | выводить ли прогресс создания картинки
+    """
+    # Проверка, задан ли набор смайликов, которыми нужно рисовать
+    error_text = 'Необходимо указать либо emojis, либо emojis_dict!'
+    assert not(emojis is None and emoji_dict_name is None), error_text
+
+    # Подготовка словаря смайликов
+    if emojis is None:
+        emoji_dict = load_emoji_dict(emoji_dict_name)
+    else:
+        # Для построения словаря используется то же разрешение
+        # (размер смайлика), что и для итоговой картинки, но
+        # это необязательно, можно просто установить константу
+        emoji_dict = create_emoji_dict(emojis=emojis.split(' '), 
+                                       background_color=None,
+                                       emoji_resolution=emoji_resolution, 
+                                       disp=disp)
+    
+    # Загрузка картинки-исходника
+    image = Image.open(f'input/{image_filename}')
+
+    # Вывод размеров картинки до уменьшения
+    if disp:
+        source_w, source_h = image.size
+        print(f'Размер картинки: {source_h}x{source_w}', end='')
+        
+    # Уменьшение картинки, преобразование в numpy
+    # и сохранение только трех каналов под R, G, B
+    image.thumbnail((emoji_width, -1))
+    image = np.array(image)[:, :, :3]
+    
+    # Размеры матрицы смайликов
+    img_h, img_w, _ = image.shape
+    # Размеры итоговой картинки
+    result_h, result_w = img_h*emoji_resolution, img_w*emoji_resolution
+    
+    # Вывод размеров картинки после уменьшения
+    if disp:
+        print(f' → {img_h}x{img_w}')
+
+    # Текстовое представление картинки из смайликов
+    emoji_image_text = ''
+
+    # Установка цвета для итоговой картинки
+    if background_color is None:
+        background_RGBA = (0, 0, 0, 0)
+    else:
+        background_RGBA = (*background_color, 255)
+        
+    # Создание итогового изображения result_image
+    with Image.new('RGBA', (result_w, result_h), background_RGBA) as result_image:
+        # Компонент для рисования на картинке
+        img_draw = ImageDraw.Draw(result_image)
+        # Шрифт для отображения смайликов
+        font = ImageFont.truetype('fonts/arial.ttf', emoji_resolution)
+        with Pilmoji(result_image) as pilmoji:
+            for i in range(img_h):
+                for j in range(img_w):
+                    # Индексы пикселей в итоговой картинке
+                    h, w = i * emoji_resolution, j * emoji_resolution
+
+                    if dynamic_background:
+                        # Поиск подходящего сочетания смайлика с фоном
+                        emoji, back = get_nearest_emoji_and_back(image[i, j], emoji_dict)
+                        # Отрисовка прямоугольника (фона для смайлика)
+                        img_draw.rectangle((w, h, w+emoji_resolution, h+emoji_resolution), 
+                                           fill=tuple(np.rint(back).astype(int)))
+                    else:
+                        # Поиск ближайшего по среднему цвету смайлика
+                        emoji = get_nearest_emoji(image[i, j], emoji_dict)
+                        
+                    # Отрисовка смайлика
+                    pilmoji.text((w, h), emoji, font=font)
+                    # Добавление смайлика в текстовое представление картинки
+                    emoji_image_text += emoji
+                    
+                    # Вывод прогресса
+                    if disp:
+                        total_px = img_h * img_w
+                        current_px = i * img_w + j + 1
+                        progress = f'{round(current_px / total_px * 100, 2)}%'
+                        print(f'\rРисование: {progress:<7}', end='')
+                        
+                # Перенос строки в текстовом представлении картинки из смайликов
+                if i + 1 < img_h:
+                    emoji_image_text += '\n'
+                    
+            # Перенос строки из-за вывода прогресса с '\r'
+            if disp:
+                print()
+
+    # Сохранение картинки в виде текста
+    with open(f'output/{save_image_name}.txt', 'w') as file:
+        file.write(emoji_image_text)
+    
+    # Сохранение изображения
+    result_image.save(f'output/{save_image_name}.png')
+    
+    if disp:
+        print('Изображение сохранено в папку output')
+        
+    return result_image
